22eac26a2e8224f457c0900bb7780003466cf356
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-23 17:53:13 +0600

    json introduction

---
+++ json.js
@@ -0,0 +1,8 @@
+const user = {id: 1, name: "Nahin", specialization: "Web Developer"};
+// JavaScript Object Notation (JSON)
+const stringyfied = JSON.stringify(user);
+console.log (user);
+console.log (stringyfied);
+
+// { id: 1, name: 'Nahin', specialization: 'Web Developer' } -> JS Object
+// {"id":1,"name":"Nahin","specialization":"Web Developer"} -> JSON (strings of the object)
