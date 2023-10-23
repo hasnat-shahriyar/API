aab43410c13b9c2a0bf326e15369cde33774b9d1
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-10-23 18:02:25 +0600

    converting a javascript object to json string

--- json.js
+++ json.js
@@ -6,3 +6,20 @@ console.log (stringyfied);
 
 // { id: 1, name: 'Nahin', specialization: 'Web Developer' } -> JS Object
 // {"id":1,"name":"Nahin","specialization":"Web Developer"} -> JSON (strings of the object)
+
+// Example 
+const shop = {
+    owner : "Hasnat",
+    address : {
+        area : "Farmgate",
+        city : "Dhaka",
+        country : "Bangladesh"
+    },
+    products : ["laptop", "keyboard", "headphone", "monitor"],
+    revenue : 45000,
+    isOpen : true,
+    isNew : false,
+}
+console.log(shop);
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
